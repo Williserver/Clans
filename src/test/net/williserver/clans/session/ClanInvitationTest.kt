568a797cbb89e8724c2ed76b0d050e938b376569
Will Morris <morris.william.work@gmail.com>
2025-04-17 17:05:56 -0400

    Add test that invitations expire

--- src/test/net/williserver/clans/session/ClanInvitationTest.kt
+++ src/test/net/williserver/clans/session/ClanInvitationTest.kt
@@ -2,8 +2,10 @@ package net.williserver.clans.session
 
 import net.williserver.clans.model.Clan
 import net.williserver.clans.session.invite.TimedClanInvitation
+import org.junit.jupiter.api.Assertions.assertFalse
 import org.junit.jupiter.api.Assertions.assertThrows
 import org.junit.jupiter.api.Test
+import java.lang.Thread.sleep
 import java.util.*
 
 class ClanInvitationTest {
@@ -22,5 +24,18 @@ class ClanInvitationTest {
         val timer = ConfirmTimer(1)
         timer.startTimer()
         assertThrows(IllegalStateException::class.java) { TimedClanInvitation(UUID.randomUUID(), clan, timer) }
+    }
+
+    @Test
+    fun testValidTimedInvitation() {
+        val leader = UUID.randomUUID()
+        val clan = Clan("TestClan", leader, mutableListOf(leader))
+        // One invitation will be created with a 10 second long timer
+        // this is more than enough time for the invite to be valid.
+        val longTimer = ConfirmTimer(10)
+        assert(TimedClanInvitation(UUID.randomUUID(), clan, longTimer).validInvite())
+        val shortTimer = ConfirmTimer(0)
+        val expiredInvite = TimedClanInvitation(UUID.randomUUID(), clan, shortTimer)
+        assertFalse(expiredInvite.validInvite())
     }
 }
