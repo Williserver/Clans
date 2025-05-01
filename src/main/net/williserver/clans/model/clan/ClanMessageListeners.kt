71740224df5169eb4bb26bda0d0716421be95d44
Will Morris <morris.william.work@gmail.com>
2025-05-01 18:12:10 -0400

    Message kicked players when they're online

--- src/main/net/williserver/clans/model/clan/ClanMessageListeners.kt
+++ src/main/net/williserver/clans/model/clan/ClanMessageListeners.kt
@@ -3,6 +3,7 @@ package net.williserver.clans.model.clan
 import net.kyori.adventure.text.Component
 import net.kyori.adventure.text.format.NamedTextColor
 import net.williserver.clans.commands.sendClanMessage
+import net.williserver.clans.commands.sendPrefixedMessage
 import net.williserver.clans.session.ClanLifecycleListener
 import org.bukkit.Bukkit
 
@@ -12,7 +13,8 @@ import org.bukkit.Bukkit
 fun constructJoinMessageListener(): ClanLifecycleListener = { clan, _, joiningPlayer ->
     // Getting offline player in case they leave instantly after joining.
     val message = prefix(clan.name)
-        .append(Component.text("${Bukkit.getOfflinePlayer(joiningPlayer).name} has joined the clan!", NamedTextColor.GREEN))
+        .append(Component.text("${Bukkit.getOfflinePlayer(joiningPlayer).name} has joined the clan!",
+            NamedTextColor.GREEN))
     sendClanMessage(clan, message)
 }
 
@@ -21,7 +23,8 @@ fun constructJoinMessageListener(): ClanLifecycleListener = { clan, _, joiningPl
  */
 fun constructLeaveMessageListener(): ClanLifecycleListener = { clan, _, leavingPlayer ->
     val message = prefix(clan.name)
-        .append(Component.text("${Bukkit.getOfflinePlayer(leavingPlayer).name} has left the clan.", NamedTextColor.DARK_RED))
+        .append(Component.text("${Bukkit.getOfflinePlayer(leavingPlayer).name} has left the clan.",
+            NamedTextColor.DARK_RED))
     sendClanMessage(clan, message)
 }
 
@@ -30,8 +33,15 @@ fun constructLeaveMessageListener(): ClanLifecycleListener = { clan, _, leavingP
  */
 fun constructKickMessageListener(): ClanLifecycleListener = { clan, agent, kickedPlayer ->
     val message = prefix(clan.name)
-        .append(Component.text("${Bukkit.getOfflinePlayer(kickedPlayer).name} has been kicked from the clan by ${Bukkit.getOfflinePlayer(agent).name}", NamedTextColor.DARK_RED))
+        .append(Component.text(
+            "${Bukkit.getOfflinePlayer(kickedPlayer).name} has been kicked from the clan by ${Bukkit.getOfflinePlayer(agent).name}",
+            NamedTextColor.DARK_RED))
     sendClanMessage(clan, message)
+
+    // If the kicked player is online, send them a message.
+    Bukkit.getPlayer(kickedPlayer)?.
+        let { player -> sendPrefixedMessage(player, Component.text("You were kicked from clan ${clan.name}!",
+            NamedTextColor.DARK_RED)) }
 }
 
 /**
