7c1623a24dc3407f18b54e9d957abe295a02c246
Will Morris <morris.william.work@gmail.com>
2025-05-09 13:37:48 -0400

    Refactor config onEnable to account for increased size

--- src/main/net/williserver/clans/ClansPlugin.kt
+++ src/main/net/williserver/clans/ClansPlugin.kt
@@ -5,11 +5,8 @@ import net.williserver.clans.commands.ClansTabCompleter
 import net.williserver.clans.commands.chat.ChatCommand
 import net.williserver.clans.commands.chat.ChatTabCompleter
 import net.williserver.clans.integration.*
-import net.williserver.clans.model.ClanSet
-import net.williserver.clans.model.ClansConfigLoader
+import net.williserver.clans.model.*
 import net.williserver.clans.model.clan.*
-import net.williserver.clans.model.readFromFile
-import net.williserver.clans.model.writeToFile
 import net.williserver.clans.session.ClanEvent.*
 import net.williserver.clans.session.ClanEventBus
 import net.williserver.clans.session.ClanListenerType.*
@@ -30,67 +27,61 @@ class ClansPlugin : JavaPlugin() {
     private val path = "$dataFolder${File.separator}clans.json"
     // clanList model.
     private lateinit var clanSet: ClanSet
+    // event bus.
+    private lateinit var bus: ClanEventBus
 
     override fun onEnable() {
+        /*
+         * Read base config
+         */
         saveDefaultConfig() // If file deleted, restore defaults
-        val config = ClansConfigLoader(logger, config).config
+        val clansConfig = ClansConfigLoader(logger, config).config
         logger.info("Loaded config.")
 
-        // Read base clan list.
+        /*
+         * Initialize ClanList.
+         */
         clanSet = readFromFile(logger, path)
         logger.info("Initialized clanList.")
 
-        // Initiate this session.
+        /*
+         * Initiate clans session.
+         */
         val session = SessionManager()
         logger.info("Initialized session.")
 
-        // Register major events in clan lifecycle.
-        val bus = ClanEventBus()
-
-        // Model listeners affect persistent data
-        bus.registerListener(CREATE, MODEL, clanSet.constructCreateListener())
-        bus.registerListener(DEMOTE, MODEL, clanSet.constructDemoteListener())
-        bus.registerListener(DISBAND, MODEL, clanSet.constructDisbandListener())
-        bus.registerListener(JOIN, MODEL, clanSet.constructJoinListener())
-        bus.registerListener(LEAVE, MODEL, clanSet.constructLeaveListener())
-        bus.registerListener(PROMOTE, MODEL, clanSet.constructPromoteListener())
-        bus.registerListener(KICK, MODEL, clanSet.constructKickListener())
+        /*
+         * Begin registering listeners for major events in clan lifecycle.
+         */
+        bus = ClanEventBus()
+        /*
+         * MODEL LISTENERS affect persistent data
+         */
+        registerCoreModelListeners()
         logger.info("Registered clans data model listeners.")
-
-        // Session listeners affect temporary data, like expiring invites.
-        bus.registerListener(JOIN, SESSION, session.constructDeregisterInviteListener())
+        /*
+         * SESSION LISTENERS affect temporary data, like expiring invites.
+         */
+        registerSessionListeners(session)
         logger.info("Registered clans session listeners.")
-
-        // Messaging listeners send informational messages when events occur.
-        bus.registerListener(CREATE, COSMETIC, constructCreateMessageListener())
-        bus.registerListener(DEMOTE, COSMETIC, constructDemoteMessageListener())
-        bus.registerListener(DISBAND, COSMETIC, constructDisbandMessageListener())
-        bus.registerListener(JOIN, COSMETIC, constructJoinMessageListener())
-        bus.registerListener(LEAVE, COSMETIC, constructLeaveMessageListener())
-        bus.registerListener(PROMOTE, COSMETIC, constructPromoteMessageListener())
-        bus.registerListener(KICK, COSMETIC, constructKickMessageListener())
+        /*
+         * COSMETIC LISTENERS send informational messages when events occur.
+         */
+        registerCosmeticListeners()
         logger.info("Registered clans cosmetic listeners.")
-
-        // Integration listeners connect clans with other features of the plugin.
-        if (config.scoreboardTeamsIntegration) {
+        /*
+         * INTEGRATION LISTENERS connect clans with other plugins.
+         */
+        if (clansConfig.scoreboardTeamsIntegration) {
             logger.info("Scoreboard teams integration enabled, registering teams listeners.")
-            val teamIntegrator = ScoreboardTeamIntegrator(logger)
-            bus.registerListener(CREATE, INTEGRATION, teamIntegrator.constructCreateAddTeamListener())
-            bus.registerListener(DISBAND, INTEGRATION, teamIntegrator.constructDisbandRemoveTeamListener())
-            bus.registerListener(JOIN, INTEGRATION, teamIntegrator.constructJoinTeamListener())
-            bus.registerListener(LEAVE, INTEGRATION, teamIntegrator.constructLeaveTeamListener())
-            // From the limited perspective of vanilla teams, a kick is just a player leaving.
-            // They lack sufficient context to impose stricter checks and so use the same leave listener.
-            bus.registerListener(KICK, INTEGRATION, teamIntegrator.constructLeaveTeamListener())
+            registerScoreboardTeamsIntegrationListeners()
         } else {
             logger.info("Scoreboard teams integration disabled, not registering teams listeners.")
         }
-
-        // initiate luckperms integration, if plugin present.
-        if (config.luckpermsIntegration && server.pluginManager.isPluginEnabled("LuckPerms")) {
+        // Luckperms integration listeners.
+        if (clansConfig.luckpermsIntegration && server.pluginManager.isPluginEnabled("LuckPerms")) {
             logger.info("LuckPerms integration enabled, registering LP listeners.")
-            logger.info("Using trackname: ${config.luckPermsTrackName}.")
-            LuckPermsIntegrator(logger, config.luckPermsTrackName).initiateTrack()
+            registerLuckPermsIntegrationListeners(clansConfig)
         } else {
             logger.info("LuckPerms integration disabled, not registering LP listeners.")
         }
@@ -98,13 +89,18 @@ class ClansPlugin : JavaPlugin() {
 
         logger.info("Finished registering clan lifecycle listeners.")
 
-        // Register commands.
+        /*
+         * Register commands
+         */
         this.getCommand("cc")!!.setExecutor(ChatCommand(clanSet))
-        this.getCommand("clans")!!.setExecutor(ClansCommand(clanSet, config, session, bus))
+        this.getCommand("clans")!!.setExecutor(ClansCommand(clanSet, clansConfig, session, bus))
         this.getCommand("cc")!!.tabCompleter = ChatTabCompleter()
         this.getCommand("clans")!!.tabCompleter = ClansTabCompleter(clanSet)
         logger.info("Registered commands.")
 
+        /*
+         * Finish enabling
+         */
         logger.info("Enabled.")
     }
 
@@ -113,5 +109,71 @@ class ClansPlugin : JavaPlugin() {
         writeToFile(logger, path, clanSet)
 
         logger.info("Disabled.")
+    }
+
+    // ***** LISTENER REGISTRATION HELPERS ***** //
+
+    /**
+     * Register listeners that affect the underlying persistent clans data model.
+     *
+     * These listeners will be run first, since the persistent data model has stringent data integrity guarantees
+     * that should be met before other listeners fire.
+     */
+    private fun registerCoreModelListeners() {
+        bus.registerListener(CREATE, MODEL, clanSet.constructCreateListener())
+        bus.registerListener(DEMOTE, MODEL, clanSet.constructDemoteListener())
+        bus.registerListener(DISBAND, MODEL, clanSet.constructDisbandListener())
+        bus.registerListener(JOIN, MODEL, clanSet.constructJoinListener())
+        bus.registerListener(LEAVE, MODEL, clanSet.constructLeaveListener())
+        bus.registerListener(PROMOTE, MODEL, clanSet.constructPromoteListener())
+        bus.registerListener(KICK, MODEL, clanSet.constructKickListener())
+    }
+
+    /**
+     * Register listeners that affect session-level data, like timers.
+     * Since this data will persist only for a single session, these listeners are run third.
+     */
+    private fun registerSessionListeners(session: SessionManager) {
+        bus.registerListener(JOIN, SESSION, session.constructDeregisterInviteListener())
+    }
+
+    /**
+     * Register listeners that message users when changes occur in the clan lifecycle.
+     *
+     * These listeners will be run last, since they should have no persistent data impact.
+     */
+    private fun registerCosmeticListeners() {
+        bus.registerListener(CREATE, COSMETIC, constructCreateMessageListener())
+        bus.registerListener(DEMOTE, COSMETIC, constructDemoteMessageListener())
+        bus.registerListener(DISBAND, COSMETIC, constructDisbandMessageListener())
+        bus.registerListener(JOIN, COSMETIC, constructJoinMessageListener())
+        bus.registerListener(LEAVE, COSMETIC, constructLeaveMessageListener())
+        bus.registerListener(PROMOTE, COSMETIC, constructPromoteMessageListener())
+        bus.registerListener(KICK, COSMETIC, constructKickMessageListener())
+    }
+
+    /**
+     * Register listeners that map clans to vanilla scoreboard teams.
+     *
+     * This class of listener is run second, since scoreboard teams are persistent.
+     */
+    private fun registerScoreboardTeamsIntegrationListeners() {
+        val teamIntegrator = ScoreboardTeamIntegrator(logger)
+        bus.registerListener(CREATE, INTEGRATION, teamIntegrator.constructCreateAddTeamListener())
+        bus.registerListener(DISBAND, INTEGRATION, teamIntegrator.constructDisbandRemoveTeamListener())
+        bus.registerListener(JOIN, INTEGRATION, teamIntegrator.constructJoinTeamListener())
+        bus.registerListener(LEAVE, INTEGRATION, teamIntegrator.constructLeaveTeamListener())
+        // From the limited perspective of vanilla teams, a kick is just a player leaving.
+        // They lack sufficient context to impose stricter checks and so use the same leave listener.
+        bus.registerListener(KICK, INTEGRATION, teamIntegrator.constructLeaveTeamListener())
+    }
+
+    /**
+     * Register listeners that map clans to LuckPerms groups.
+     *
+     * This class of listener is run second, since LuckPerms groups are persistent.
+     */
+    private fun registerLuckPermsIntegrationListeners(clansConfig: ClansConfig) {
+        LuckPermsIntegrator(logger, clansConfig.luckPermsTrackName).initiateTrack()
     }
 }
--- src/main/net/williserver/clans/integration/LuckPermsIntegrator.kt
+++ src/main/net/williserver/clans/integration/LuckPermsIntegrator.kt
@@ -2,10 +2,7 @@ package net.williserver.clans.integration
 
 import net.luckperms.api.LuckPerms
 import net.williserver.clans.LogHandler
-import net.williserver.clans.model.clan.Clan
-import net.williserver.clans.session.ClanLifecycleListener
 import org.bukkit.Bukkit
-import java.util.*
 
 /**
  * Integrate with LuckPerms group tracks, if the plugin is present.
@@ -23,13 +20,6 @@ class LuckPermsIntegrator(private val logger: LogHandler, private val trackName:
      */
     fun initiateTrack() {
         luckperms.trackManager.createAndLoadTrack(trackName)
-        logger.info("$LOG_PREFIX: Loading clans track.")
-    }
-
-    /**
-     * Generate a listener to register a new rank when a clan is created.
-     */
-    fun generateCreateListener(): ClanLifecycleListener = { clan: Clan, uuid: UUID, uuid1: UUID ->
-
+        logger.info("$LOG_PREFIX: Loading clans track under name \"$trackName\".")
     }
 }
